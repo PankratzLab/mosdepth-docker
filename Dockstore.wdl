298fe3e0714991738804489349bb18700c367b47
jlanej <jlanej@gmail.com>
2021-09-01 16:27:23 -0500

    test

---
+++ Dockstore.wdl
@@ -0,0 +1,36 @@
+version 1.0
+task mosdepth {
+    input {
+        File bam_or_cram_input
+        String outputRoot
+        File referenceGenome
+        Int mem_gb
+    }
+
+	command {	
+		bash /usr/local/bin/mosdepth -n -t 1 --by 1000 --fasta ${referenceGenome} ${outputRoot} ${bam_or_cram_input}
+	}
+
+	output {
+		File coverageBed = "~{outputRoot}.bed.gz"
+	}
+
+	runtime {
+		docker: "quay.io/jlanej/mosdepth-docker:v0.3.2"
+		memory: mem_gb + "GB"	}
+
+	meta {
+		author: "jlanej"
+	}
+}
+
+workflow mosdepthWorkflow {
+    input {
+        File bam_or_cram_input
+        String outputRoot
+        File referenceGenome
+        Int mem_gb
+    }
+	call mosdepth { input: bam_or_cram_input=bam_or_cram_input,outputRoot=outputRoot,referenceGenome=referenceGenome, mem_gb=mem_gb }
+}
+
--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/bin/bash/home/pankrat2/public/bin/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa
 
-
+rsync -avzP msi:/home/pankrat2/public/bin/ref/GRCh38_full_analysis_set_plus_decoy_hla.* /Volumes/Beta2/resources/
 [![Docker Repository on Quay](https://quay.io/repository/jlanej/mosdepth-docker/status "Docker Repository on Quay")](https://quay.io/repository/jlanej/mosdepth-docker)
-docker build -t quay.io/repository/jlanej/mosdepth-docker:v0.3.2 .
+docker build -t quay.io/jlanej/mosdepth-docker:v0.3.2 .
+
+
+docker build -t mosdepth-docker .
+
+
+cd /Volumes/Beta2/resources/1000G/
+
+dockstore tool launch --local-entry /Users/Kitty/git/mosdepth-docker/Dockstore.wdl --json /Users/Kitty/git/mosdepth-docker/test.input.json
+
+		docker: "quay.io/jlanej/mosdepth-docker:v0.3.2"
